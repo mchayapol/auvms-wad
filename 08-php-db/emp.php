b8b0d1e1605860f8d4645194c5803b7674f04bed
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-01-04 15:18:43 +0700

    Restructure

--- 08-php-db/emp.php
+++ 08-php-db/emp.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "admin";
-$dbname = "employees";
+require_once("config.php");
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -11,6 +8,10 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+?>
+<h1>Employees</h1>
+<?php
+// Employee
 $sql = "SELECT * FROM employees LIMIT 10";
 $result = mysqli_query($conn, $sql);
 
@@ -18,13 +19,34 @@ if (mysqli_num_rows($result) > 0) {
   // output data of each row
   $counter = 0;
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["emp_no"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+    echo "id: " . $row["emp_no"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. " - ".$row["gender"]."<br>";
     $counter++;
   }
   echo "$counter results";
 } else {
   echo "0 results";
 }
+?>
+<hr/>
+<h1>Departments</h1>
+<?php
+
+// Departments
+$sql = "SELECT * FROM departments LIMIT 10";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  $counter = 0;
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["dept_no"]. " - Name: " . $row["dept_name"]."<br>";
+    $counter++;
+  }
+  echo "$counter results";
+} else {
+  echo "0 results";
+}
+
 
 mysqli_close($conn);
 
