97e25c77e8c4a5dffe7971827dd349a897428a65
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-02-12 10:20:21 +0700

    Added form manipulation example

---
+++ 12-spa-react/form-manipulation/src/App.js
@@ -0,0 +1,14 @@
+import RegistrationForm from './RegistrationForm'
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <RegistrationForm />
+    </Container>
+  );
+}
+
+export default App;
