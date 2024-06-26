0c020792d47992804441ad80a9a7fe7d9eaabe65
Chayapol Moemeng <mchayapol@gmail.com>
2024-06-26 20:23:21 +0700

    Added samples

--- carada-app/src/routes/root.jsx
+++ carada-app/src/routes/root.jsx
@@ -40,7 +40,7 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1>Carada</h1>
         <div>
           <Form id="search-form" role="search">
             <input
